9386011bfbb8ddf39480cbcda415f5430d45491d
Uchujin <spec273.15c@gmail.com>
2026-02-13 11:05:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -329,7 +329,7 @@ if(rc.length>0)detectActivity(rc)}
 const ACTIVITIES={
 crypto:{
   frames:['/static/crypto_1.png','/static/crypto_2.png','/static/crypto_3.png','/static/crypto_4.png'],
-  left:'28%',top:'18%',width:'15%'
+  left:'39.2%',top:'21%',width:'15%'
 },
 study:{
   frames:['/static/study_1.png','/static/study_2.png','/static/study_3.png'],
